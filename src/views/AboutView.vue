a4b52050ee89f8f697edfc9b1843cbcc904f8708
Butters <swki418@gmail.com>
2022-03-19 22:16:23 +0800

    git init

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,4 +2,16 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <MyCard></MyCard>
 </template>
+
+<script>
+// @ is an alias to /src
+import MyCard from '@/components/MyCard.vue'
+
+export default {
+  components: {
+    MyCard
+  }
+}
+</script>
